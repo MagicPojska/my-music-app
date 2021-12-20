b529de51072f7c18717460daa79051c66692f857
MagicPojska <pojskicsafet@gmail.com>
2021-12-20 11:43:13 +0100

    Added model for Kategorija

---
+++ server/my-music-app-api/my-music-app-api/Models/Kategorija.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace my_music_app_api.Models
+{
+    public class Kategorija
+    {
+        public int Id { get; set; }
+        
+        [StringLength(30)]
+        public string NazivKategorije { get; set; } = String.Empty;
+    }
+}
