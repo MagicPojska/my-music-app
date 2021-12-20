3ab81ac59e72b48831a62bb67f82d4fce4bfbd0b
MagicPojska <pojskicsafet@gmail.com>
2021-12-20 12:34:14 +0100

    Added model for Pjesma and DataContext.cs file

---
+++ server/my-music-app-api/my-music-app-api/Data/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace my_music_app_api.Data
+{
+    public class DataContext : DbContext
+    {
+
+    }
+}
---
+++ server/my-music-app-api/my-music-app-api/Models/Pjesma.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace my_music_app_api.Models
+{
+    public class Pjesma
+    {
+        public int Id { get; set; }
+
+        [StringLength(30)]
+        public string NazivPjesme { get; set; } = String.Empty;
+
+        [StringLength(50)]
+        public string NazivIzvodjaca { get; set;} = String.Empty;
+
+        [StringLength(150)]
+        public string UrlPjesme { get; set; } = String.Empty;
+
+        public int Ocjena { get; set; }
+        public bool IsFavorite { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime DatumUnosa { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime DatumModifikovanja { get; set; }
+
+        public int KategorijaId { get; set; }
+        public Kategorija Kategorija { get; set; }
+
+
+        public Pjesma()
+        {
+            IsFavorite = false;
+            DatumUnosa = DateTime.Today;
+        }
+    }
+}
