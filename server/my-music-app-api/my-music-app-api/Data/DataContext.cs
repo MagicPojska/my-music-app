877be1ed6ee45d89e8cfe2f9566d8e85e93580b2
MagicPojska <pojskicsafet@gmail.com>
2021-12-20 12:35:02 +0100

    Added data context for kategorija and pjesma models

--- server/my-music-app-api/my-music-app-api/Data/DataContext.cs
+++ server/my-music-app-api/my-music-app-api/Data/DataContext.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using my_music_app_api.Models;
 
 namespace my_music_app_api.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
+        public DbSet<Kategorija> Kategorije { get; set; }
+
+        public DbSet<Pjesma> Pjesme { get; set;}
     }
 }
