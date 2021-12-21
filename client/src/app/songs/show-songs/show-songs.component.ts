bf8c086dd69138a21d160c279b751f7821e94fe5
MagicPojska <pojskicsafet@gmail.com>
2021-12-21 10:44:30 +0100

    Added a basic table to test if everything works when fetching data from API

--- client/src/app/songs/show-songs/show-songs.component.ts
+++ client/src/app/songs/show-songs/show-songs.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SongsApiService } from 'src/app/songs-api.service';
 
 @Component({
   selector: 'app-show-songs',
   templateUrl: './show-songs.component.html',
-  styleUrls: ['./show-songs.component.css']
+  styleUrls: ['./show-songs.component.css'],
 })
 export class ShowSongsComponent implements OnInit {
+  songsList$!: Observable<any[]>;
+  categoryList$!: Observable<any[]>;
 
-  constructor() { }
+  // Map to display data with foreign keys
+  categoryListMap: Map<number, string> = new Map();
+
+  constructor(private service: SongsApiService) {}
 
   ngOnInit(): void {
+    this.songsList$ = this.service.getSongsList();
   }
-
 }
--- server/my-music-app-api/my-music-app-api/Program.cs
+++ server/my-music-app-api/my-music-app-api/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: myAllowSpecificOrigins,
     builder =>
     {
-        builder.WithOrigins("http://loclahost:4200")
+        builder.WithOrigins("http://localhost:4200")
         .AllowAnyMethod()
         .AllowAnyHeader();
     });
