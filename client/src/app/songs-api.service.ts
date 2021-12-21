843660a914c841e2f432fd6aa79b29e1d9487ab0
MagicPojska <pojskicsafet@gmail.com>
2021-12-21 10:19:25 +0100

    Added GET, POST, DELETE and UPDATE song API requests

--- client/src/app/songs-api.service.ts
+++ client/src/app/songs-api.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SongsApiService {
+  readonly songsAPIUrl = 'https://localhost:7042/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getSongsList(): Observable<any[]> {
+    return this.http.get<any>(this.songsAPIUrl + '/pjesma');
+  }
+
+  addSongs(data: any) {
+    return this.http.post(this.songsAPIUrl + '/pjesma', data);
+  }
+
+  updateSong(id: number, data: any) {
+    return this.http.put(this.songsAPIUrl + `/pjesma/${id}`, data);
+  }
+
+  deleteSong(id: number) {
+    return this.http.delete(this.songsAPIUrl + `/pjesma/${id}`);
+  }
 }
